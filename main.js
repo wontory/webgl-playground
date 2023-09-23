73b7258c358eb807fefa36531047e8a35b747c05
조성원 <tjddnjsjo@kakao.com>
2023-09-24 00:41:49 +0900

    init: Set b.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import a from './src/a.js';
+import b from './src/b.js';
 import './main.css';
 
-a();
+// a();
+b();
---
+++ src/b.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+const a = () => {
+  // Create a scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xf5f5fa);
+
+  // Create a camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const render = (time) => {
+    time *= 0.001;
+
+    // cube.rotation.x = time;
+    // cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
+};
+
+export default a;
