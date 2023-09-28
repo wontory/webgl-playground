f8d61160a0b2a091a1fbe854085adeaf07dd39d1
조성원 <tjddnjsjo@kakao.com>
2023-09-28 22:44:09 +0900

    feat: Lights & materials

--- src/c.js
+++ src/c.js
@@ -17,8 +17,12 @@ const c = () => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-
   document.body.appendChild(renderer.domElement);
+
+  // Create lights
+  const pointLight = new THREE.PointLight(0xffffff, 500);
+  pointLight.position.set(0, 2, 12);
+  scene.add(pointLight);
 
   // Create meshes
   const geometry = new THREE.TorusGeometry(0.3, 0.15, 16, 40);
@@ -27,32 +31,56 @@ const c = () => {
   obj01.position.x = -2;
   scene.add(obj01);
 
-  const material02 = new THREE.MeshBasicMaterial({ color: 0xff7f00 });
+  const material02 = new THREE.MeshStandardMaterial({
+    color: 0xff7f00,
+    metalness: 0.6,
+    roughness: 0.4,
+    // wireframe: true,
+    // transparent: true,
+    // opacity: 0.5,
+  });
   const obj02 = new THREE.Mesh(geometry, material02);
   obj02.position.x = -1;
   scene.add(obj02);
 
-  const material03 = new THREE.MeshBasicMaterial({ color: 0xff7f00 });
+  const material03 = new THREE.MeshPhysicalMaterial({
+    color: 0xff7f00,
+    clearcoat: 1,
+    clearcoatRoughness: 0.1,
+  });
   const obj03 = new THREE.Mesh(geometry, material03);
   obj03.position.x = 0;
   scene.add(obj03);
 
-  const material04 = new THREE.MeshBasicMaterial({ color: 0xff7f00 });
+  const material04 = new THREE.MeshLambertMaterial({ color: 0xff7f00 });
   const obj04 = new THREE.Mesh(geometry, material04);
   obj04.position.x = 1;
   scene.add(obj04);
 
-  const material05 = new THREE.MeshBasicMaterial({ color: 0xff7f00 });
+  const material05 = new THREE.MeshPhongMaterial({
+    color: 0xff7f00,
+    shininess: 60,
+    specular: 0x004fff,
+  });
   const obj05 = new THREE.Mesh(geometry, material05);
   obj05.position.x = 2;
   scene.add(obj05);
 
-  const render = () => {
+  const render = (time) => {
+    time *= 0.001;
+
+    obj01.rotation.y += 0.01;
+    obj02.rotation.y += 0.01;
+    obj03.rotation.y += 0.01;
+    obj04.rotation.y += 0.01;
+    obj05.rotation.y += 0.01;
+
     renderer.render(scene, camera);
+
     requestAnimationFrame(render);
   };
 
-  render();
+  requestAnimationFrame(render);
 
   // Resize window
   const onWindowResize = () => {
