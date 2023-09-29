d604d8fba72c0c2b8a65f14c0210c7f4f807b50c
조성원 <tjddnjsjo@kakao.com>
2023-09-29 23:39:23 +0900

    feat: Textures

--- src/d.js
+++ src/d.js
@@ -30,24 +30,54 @@ const d = () => {
   pointLight.position.set(0, 2, 12);
   scene.add(pointLight);
 
+  // Add textures
+  const textureLoader = new THREE.TextureLoader();
+  const textureBaseColor = textureLoader.load(
+    '../static/img/Stone_Path_008_basecolor.jpg'
+  );
+  const textureNormalMap = textureLoader.load(
+    '../static/img/Stone_Path_008_normal.jpg'
+  );
+  const textureHeightMap = textureLoader.load(
+    '../static/img/Stone_Path_008_height.png'
+  );
+  const textureRoughnessMap = textureLoader.load(
+    '../static/img/Stone_Path_008_roughness.jpg'
+  );
+
   // Create meshes
-  const geometry = new THREE.SphereGeometry(0.3, 32, 16);
-  const material01 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
+  const geometry = new THREE.SphereGeometry(0.3, 32, 32);
+  const material01 = new THREE.MeshStandardMaterial({ map: textureBaseColor });
   const obj01 = new THREE.Mesh(geometry, material01);
   obj01.position.x = -1.5;
   scene.add(obj01);
 
-  const material02 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
+  const material02 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalMap,
+  });
   const obj02 = new THREE.Mesh(geometry, material02);
   obj02.position.x = -0.5;
   scene.add(obj02);
 
-  const material03 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
+  const material03 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalMap,
+    displacementMap: textureHeightMap,
+    displacementScale: 0.03,
+  });
   const obj03 = new THREE.Mesh(geometry, material03);
   obj03.position.x = 0.5;
   scene.add(obj03);
 
-  const material04 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
+  const material04 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalMap,
+    displacementMap: textureHeightMap,
+    displacementScale: 0.03,
+    roughnessMap: textureRoughnessMap,
+    ropughness: 0.8,
+  });
   const obj04 = new THREE.Mesh(geometry, material04);
   obj04.position.x = 1.5;
   scene.add(obj04);
