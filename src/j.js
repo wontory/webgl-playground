38d87b4dd643b214d72d4a2cd8dc6e61c6115701
조성원 <tjddnjsjo@kakao.com>
2023-10-06 20:57:40 +0900

    feat: Sky box 이해하기

--- src/j.js
+++ src/j.js
@@ -6,6 +6,10 @@ const j = () => {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0xeeeeee);
 
+  // Create a axesHelper
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+
   // Create a camera
   const camera = new THREE.PerspectiveCamera(
     50,
@@ -28,18 +32,35 @@ const j = () => {
   controls.maxDistance = 800;
   controls.update();
 
-  const texture = new THREE.TextureLoader().load('./img/bay_rt.jpg');
-
   // Create meshes
-  const skyGeometry = new THREE.BoxGeometry(24, 24, 24);
-  const skyMaterial = new THREE.MeshBasicMaterial({
-    color: 0x333333,
-  });
-  const sky = new THREE.Mesh(skyGeometry, skyMaterial);
+  const skyMaterialArray = [];
+  const texture_ft = new THREE.TextureLoader().load('./img/bay_ft.jpg');
+  const texture_bk = new THREE.TextureLoader().load('./img/bay_bk.jpg');
+  const texture_up = new THREE.TextureLoader().load('./img/bay_up.jpg');
+  const texture_dn = new THREE.TextureLoader().load('./img/bay_dn.jpg');
+  const texture_rt = new THREE.TextureLoader().load('./img/bay_rt.jpg');
+  const texture_lf = new THREE.TextureLoader().load('./img/bay_lf.jpg');
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_ft }));
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_bk }));
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_up }));
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_dn }));
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_rt }));
+  skyMaterialArray.push(new THREE.MeshBasicMaterial({ map: texture_lf }));
+  for (let i = 0; i < 6; i++) {
+    skyMaterialArray[i].side = THREE.BackSide;
+  }
+
+  const skyGeometry = new THREE.BoxGeometry(2400, 2400, 2400);
+  // const skyMaterial = new THREE.MeshBasicMaterial({
+  //   // color: 0x333333,
+  //   map: skyMaterialArray,
+  // });
+  // skyMaterial.side = THREE.BackSide; // 큐브의 안쪽 면에 렌더링되도록 설정
+  const sky = new THREE.Mesh(skyGeometry, skyMaterialArray);
   scene.add(sky);
 
   // Create lights
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
   scene.add(ambientLight);
 
   const animate = () => {
